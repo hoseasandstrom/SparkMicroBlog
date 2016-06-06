50daced3bff79627c51c1a79330f5681f1b80ad6
hoseasandstrom <Lakyn4ever>
2016-06-06 14:00:58 -0400

    Added functionality

--- src/com/studenttheironyard/Main.java
+++ src/com/studenttheironyard/Main.java
@@ -1,8 +1,51 @@
 package com.studenttheironyard;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
+    static User user;
+    static ArrayList<User> userList = new ArrayList<>();
 
     public static void main(String[] args) {
-	// write your code here
+        Spark.init();
+        Spark.get(
+                "/",
+                (request, response) -> {
+            HashMap content = new HashMap();
+            if (user == null) {
+                return new ModelAndView(content, "login.html");
+            } else {
+                content.put("name", user.name);
+                content.put("users", userList);
+                return new ModelAndView(content, "index.html");
+            }
+        },
+            new MustacheTemplateEngine()
+    );
+    Spark.post(
+            "/login",
+            (request, response) -> {
+            String username = request.queryParams("username");
+            user = new User(username);
+            userList.add(user);
+            response.redirect("/");
+            return "";
+            }
+        );
+
+
+    Spark.post (
+            "/logout",
+            (request, response) -> {
+            user = null;
+            response.redirect("/");
+            return "";
+            }
+        );
     }
 }
